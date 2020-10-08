a3ce66c29b976c9529b68e7acff0e2f9cd0120f0
Ruslan Lukianenko <ruslan201010@gmail.com>
2020-10-08 11:57:18 +0300

    logger moved in development

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
 import rootReducer from './root.reducer';
 
-const middlewares = [logger];
+const middlewares = process.env.NODE_ENV === 'development' ? [logger] : [];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
