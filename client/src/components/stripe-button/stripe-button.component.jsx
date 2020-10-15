6926e19d5845c192669ca232abe3e84c12183462
Ruslan Lukianenko <ruslan201010@gmail.com>
2020-10-15 16:10:27 +0300

    server added

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import StripeCheckout from 'react-stripe-checkout';
 import { StripeContainer, WarningMessage } from './stripe-button.styles';
 
 const STRIPE_API_KEY = 'pk_test_51HZEXKHQarsc8i8zbqJegMNUfr8tUicgj6sXc3Vz5CgPIWea67dQxdRV2fojAFiHBdKKkY45iS75QVMDF0reMGyx00WHPhwJU1';
 
-const onToken = () => {
-  alert('Payment successful');
-};
-
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
+
+  const onToken = async (token) => {
+    try {
+      await axios.post('payment', {
+        token,
+        amount: priceForStripe,
+      });
+
+      alert('The payment has been processed!');
+    } catch (e) {
+      console.log(e);
+      alert('Something went wrong. Please make sure that you using valid credit card');
+    }
+  };
 
   return (
     <StripeContainer>
